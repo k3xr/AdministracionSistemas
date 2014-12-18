30e062357de7c5430713c75d5795d4fefc4a3ee2
k3xr <k3ther@gmail.com>
2014-12-18 23:34:14 +0100

    Added LVM script

--- Cluster/aux_lvm.sh
+++ Cluster/aux_lvm.sh
@@ -12,9 +12,35 @@ fi
 # Checks config file
 oldIFS=$IFS
 IFS="\n"
+iter=0
 while read line
 do
-	linea=$line
-	
+	if [ $iter = 0 ]; then
+		volumeGroupName=$line
+	elif [ $iter = 1 ]; then
+		deviceListInGroup=$line
+	else
+		volumes[$(($iter-2))]=$linea
+	fi    
+	let iter+=1
 done < $1
 IFS=$oldIFS
+
+ssh $2 'pvcreate $deviceListInGroup;vgcreate $volumeGroupName $deviceListInGroup'
+
+for sentence in volumes
+do
+	iter2=0
+	for word in $sentence
+	do
+		if [ $iter = 0 ]; then
+			name=$word
+		else
+			size=$word
+		fi    
+		let iter+=1
+	done
+	
+	ssh $2 'lvcreate --name $name --size $size $volumeGroupName'
+	
+done
