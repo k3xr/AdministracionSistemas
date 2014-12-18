5023e36bed06467cf89f2a6081904165b3a63342
markez92 <markez92@gmail.com>
2014-12-18 20:54:14 +0100

    Preparados

--- configurar_cluster.sh
+++ configurar_cluster.sh
@@ -11,10 +11,33 @@ fi
 
 # Aqui se comprueba que el archivo de configurado 
 IFS=" ,#"
-while read var1 var2 var3
+while read maquinaIp servicio config
 do
-    if [ ! -z $var1 ] || [ ! -z $var2 ] || [ ! -z $var3 ] 
+    if [ ! -z $maquina ] || [ ! -z $servicio ] || [ ! -z $config ] 
     then
-	echo "Linea leida: $var1  $var2 $var3"
+	echo "Linea leida: $maquina $servicio $config"
+	
+	#Ponemos un switch case para actuar segun el servicio
+	case $servicio in
+	    mount)
+			./auxiliar/aux_mount.sh $config;;
+	    raid)
+			./auxiliar/aux_raid.sh $config;;
+	    lvm)
+			./auxiliar/aux_lvm.sh $config;;
+	    nis_server)
+			./auxiliar/aux_nis_server.sh $config;;
+	    nis_client)
+			./auxiliar/aux_nis_client.sh $config;;
+	    nfs_server)
+			./auxiliar/aux_nfs_server.sh $config;;
+	    nfs_client)
+			./auxiliar/aux_nfs_client.sh $config;;
+	    backup_server)
+			./auxiliar/aux_backup_server.sh $config;;
+	    backup_client)
+			./auxiliar/aux_backup_client.sh $config;;
+	esac
+
     fi
 done < "$1"
