dc9440faf7064d2fa54a050d52b7096ff3629bce
markez92 <markez92@gmail.com>
2014-12-18 21:01:30 +0100

    Vamos

--- Cluster/configurar_cluster.sh
+++ Cluster/configurar_cluster.sh
@@ -5,17 +5,39 @@ EXPECTED_ARGS=1
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-    echo "Use: $0 <fichero_configuracion>"
+    echo "Use: $0 <fichero_configuración>"
     exit 1
 fi
 
-# Aqui se comprueba el archivo de configuracion
+# Aqui se comprueba que el archivo de configurado 
 IFS=" ,#"
-while read var1 var2 var3
+while read maquinaIp servicio config
 do
-    if [ ! -z $var1 ] || [ ! -z $var2 ] || [ ! -z $var3 ] 
+    if [ ! -z $maquina ] || [ ! -z $servicio ] || [ ! -z $config ] 
     then
-	echo "Linea leida: $var1  $var2 $var3"
+	echo "Linea leida: $maquina $servicio $config"
+	
+	#Ponemos un switch case para actuar segun el servicio
+	case $servicio in
+	    mount)
+			./auxiliar/aux_mount.sh $config;;
+	    raid)
+			./auxiliar/aux_raid.sh $config;;
+	    lvm)
+			./auxiliar/aux_lvm.sh $config;;
+	    nis_server)
+			./auxiliar/aux_nis_server.sh $config;;
+	    nis_client)
+			./auxiliar/aux_nis_client.sh $config;;
+	    nfs_server)
+			./auxiliar/aux_nfs_server.sh $config;;
+	    nfs_client)
+			./auxiliar/aux_nfs_client.sh $config;;
+	    backup_server)
+			./auxiliar/aux_backup_server.sh $config;;
+	    backup_client)
+			./auxiliar/aux_backup_client.sh $config;;
+	esac
+
     fi
 done < "$1"
-
