1b32195e5116a10ecf9588e27c983609e4141c0f
k3xr <k3ther@gmail.com>
2014-12-18 21:44:05 +0100

    Added mount aux script

---
+++ Cluster/aux_mount.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Checks number of expected args
+EXPECTED_ARGS=2
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+	echo "Error: not expected args"
+    exit 1
+fi
+
+# Checks config file
+oldIFS=$IFS
+IFS="\n"
+iter=0
+while read line
+do
+	if [ $iter = 0 ]; then
+		echo "line 1"
+		deviceName=$line
+	elif [ $iter = 1 ]; then
+		echo "line 2"
+		mountPoint=$line
+		
+		# Make changes permanent
+		ssh $2 'echo "#File system: $deviceName" >> /etc/fstab; echo "$deviceName $mountPoint auto defaults,auto,rw 0 0" >> /etc/fstab'
+		
+	else
+		echo "Error in service config file"
+		exit 1
+	fi
+    
+	let iter+=1
+done < $1
+IFS=$oldIFS
+
--- Cluster/configurar_cluster.sh
+++ Cluster/configurar_cluster.sh
@@ -5,11 +5,11 @@ EXPECTED_ARGS=1
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-    echo "Use: $0 <fichero_configuración>"
+    echo "Use: $0 <fichero_configuracion>"
     exit 1
 fi
 
-# Aqui se comprueba que el archivo de configurado 
+# Aqui se comprueba el archivo de configuracion 
 IFS=" ,#"
 while read maquinaIp servicio config
 do
@@ -20,23 +20,23 @@ do
 	#Ponemos un switch case para actuar segun el servicio
 	case $servicio in
 	    mount)
-			./auxiliar/aux_mount.sh $config;;
+			./auxiliar/aux_mount.sh $config $maquina;;
 	    raid)
-			./auxiliar/aux_raid.sh $config;;
+			./auxiliar/aux_raid.sh $config $maquina;;
 	    lvm)
-			./auxiliar/aux_lvm.sh $config;;
+			./auxiliar/aux_lvm.sh $config $maquina;;
 	    nis_server)
-			./auxiliar/aux_nis_server.sh $config;;
+			./auxiliar/aux_nis_server.sh $config $maquina;;
 	    nis_client)
-			./auxiliar/aux_nis_client.sh $config;;
+			./auxiliar/aux_nis_client.sh $config $maquina;;
 	    nfs_server)
-			./auxiliar/aux_nfs_server.sh $config;;
+			./auxiliar/aux_nfs_server.sh $config $maquina;;
 	    nfs_client)
-			./auxiliar/aux_nfs_client.sh $config;;
+			./auxiliar/aux_nfs_client.sh $config $maquina;;
 	    backup_server)
-			./auxiliar/aux_backup_server.sh $config;;
+			./auxiliar/aux_backup_server.sh $config $maquina;;
 	    backup_client)
-			./auxiliar/aux_backup_client.sh $config;;
+			./auxiliar/aux_backup_client.sh $config $maquina;;
 	esac
 
     fi
