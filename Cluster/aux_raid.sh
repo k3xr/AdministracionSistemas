defe5cd1e9ed28f09ea9096b6cd43208fdd851ed
markez92 <markez92@gmail.com>
2014-12-19 19:19:27 +0100

    Raid and mount working

--- Cluster/aux_raid.sh
+++ Cluster/aux_raid.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-echo "Leyendo el fichero de configuracion"
+#echo "Leyendo el fichero de configuracion"
 declare -a ARRAY
 
-IF=$'\n'
-while read LINE; do
+IFS=$'\n'
+while read LINE || [[ -n "$LINE" ]]; do
     ARRAY[$count]=$LINE
     ((count++))
+    
 done <"$1"
 
-echo Number of elements: ${#ARRAY[@]}
-# echo contenido del array
-echo ${ARRAY[@]}
+#echo Number of elements: ${#ARRAY[@]}
+#echo contenido del array
+#echo ${ARRAY[@]}
 
 #Array para los dispositivos
 declare -a Array_Dispositivos
-IF=$' '
+IFS=$' '
 read -a Array_Dispositivos <<< "${ARRAY[2]}"
 
-echo "Enviando comando por ssh"
+echo "Enviando comando por ssh "
+
+#Primero debemos instalar mdadm
+ssh $2 sudo apt-get install mdadm
 
 #Realizamos el envio 
-#echo 'ssh '$2 'mdadm --create --name='${ARRAY[0]}' --level='${ARRAY[1]}' --raid-devices='${#Array_Dispositivos[*]} ${ARRAY[2]}
-echo $2
-echo 'ssh '$2  'whoami'
+echo "ssh sudo $2 mdadm --create --name=${ARRAY[0]} --metadata=0.90 --level=${ARRAY[1]} --raid-devices=${#Array_Dispositivos[*]} ${ARRAY[0]} ${ARRAY[2]}"
+
+ssh $2 sudo mdadm --create --name=${ARRAY[0]} --level=${ARRAY[1]} --metadata=0.90 --raid-devices=${#Array_Dispositivos[*]} ${ARRAY[0]} ${ARRAY[2]} < /dev/null
