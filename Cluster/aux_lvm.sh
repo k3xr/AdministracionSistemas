8b9d3fd661c7e8a3edfd3a93099897924c4f6561
k3xr <k3ther@gmail.com>
2014-12-19 13:08:13 +0100

    Loop in l#38 not working

--- Cluster/aux_lvm.sh
+++ Cluster/aux_lvm.sh
@@ -27,22 +27,28 @@ done < "$1"
 IFS=$oldIFS
 
 # Install the service
-echo "Installing the service"
+echo "Installing the service -----------------------------------------------"
 ssh $2 'apt-get install -y lvm2' < /dev/null
+echo "Executing pvcreate $deviceListInGroup --------------------------------"
 ssh $2 'pvcreate $deviceListInGroup' < /dev/null 
+echo "Executing vgcreate $volumeGroupName $deviceListInGroup ---------------"
 ssh $2 'vgcreate $volumeGroupName $deviceListInGroup' < /dev/null
 
-for sentence in volumes
+echo "${volumes[0]}"
+for sentence in ${volumes[*]}
 do
-	iter2=0
-	for word in $sentence
+	echo "sentence: $sentence"
+	iter3=0
+	for word in ${sentence[$iter3]}
 	do
-		if [ $iter = 0 ]; then
+		echo "word: $iter3 : $word"
+		if [ $iter3 = 0 ]; then
 			name=$word
 		else
 			size=$word
 		fi    
-		let iter+=1
+		let iter3+=1
 	done
+	echo "Executing lvcreate --name $name --size $size $volumeGroupName ------"
 	ssh $2 'lvcreate --name $name --size $size $volumeGroupName' < /dev/null
 done
